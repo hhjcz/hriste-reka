ba2fe24dba20eb4d14cfcbaf93023fbc77b49179
Jan Haering <hhj@centrum.cz>
2016-08-18 13:38:16 +0200

    feat: add express server

---
+++ src/server.js
@@ -0,0 +1,11 @@
+/** Created by hhj on 8/18/16. */
+require('babel-register')({})
+var express = require('express')
+
+var app = express()
+
+app.use('/', express.static(__dirname + '/../public'))
+
+app.listen(8080, function() {
+  console.log('Server running on port 8080')
+})
