c5264885d8c1d9397bba4e7d2b6b9839a645b5b2
Jan Haering <hhj@centrum.cz>
2016-08-19 09:00:48 +0200

    fix: server pro from env

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 /** Created by hhj on 8/18/16. */
 // require('babel-register')({})
 var express = require('express')
+var path = require('path')
+
+// e.g. heroku.com sets env.PORT
+var PORT = process.env.PORT || 8080
 
 var app = express()
 
-app.use('/', express.static(__dirname + '/public'))
-app.use('/', express.static(__dirname + '/dist'))
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, '/dist')))
 
-app.listen(8080, function() {
-  console.log('Server running on port 8080')
+app.listen(PORT, function() {
+  console.log('Server running on port ' + PORT)
 })
