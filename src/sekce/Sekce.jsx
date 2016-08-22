a1e678420675f3f7645ae6159f3d73ddffda22fe
Jan Haering <hhj@centrum.cz>
2016-08-22 14:33:16 +0200

    feat: stop timer button

--- src/sekce/Sekce.jsx
+++ src/sekce/Sekce.jsx
@@ -14,22 +14,34 @@ export class Sekce extends React.Component {
 
   static defaultProps = {};
 
+  state = { timer: null }
+
   componentDidMount() {
-    this.timer = setInterval(this.props.increment, 1000)
+    this.startTimer()
   }
 
   componentWillUnmount() {
-    clearInterval(this.timer)
+    this.stopTimer()
   }
 
+  startTimer = () => this.setState({ timer: setInterval(this.props.increment, 500) })
+
+  stopTimer = () => this.setState({ timer: clearInterval(this.state.timer) })
+
   render() {
-    connect(this.props)
     const { hodnota, increment, decrement } = this.props
     return (
-      <div>
-        <span>A máme tu</span>
+      <div style={{ padding: '2em' }}>
         <span> {hodnota} </span>
+        <span>men went to mow...</span>
         <button onClick={increment}>+</button>
+        {
+          this.state.timer ? (
+            <button onClick={this.stopTimer}>{String.fromCharCode(9612) + String.fromCharCode(9612)}</button>
+          ) : (
+            <button onClick={this.startTimer}>{String.fromCharCode(0x25b6)}</button>
+          )
+        }
         <button onClick={decrement}>-</button>
         <div>
           <img alt="50x50 placeholder" src={require('./50x50.png')} />
