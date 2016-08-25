933f95bc226ae3e05a58c075d50da029f38b4bce
Jan Haering <hhj@centrum.cz>
2016-08-25 09:53:27 +0200

    style: url in e2e hriste

--- e2e/pages/reactApp.js
+++ e2e/pages/reactApp.js
@@ -1,6 +1,6 @@
 /** Created by hhj on 8/24/16. */
 export default {
-  url: 'http://localhost:8080',
+  url: 'http://localhost:8080/hriste',
   elements: {
     reactView: {
       selector: '#react-view'
